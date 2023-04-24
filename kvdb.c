7d5004511968c99e8d1426cd31e8173b98dc39aa
yht <haotian.yang.cs@gmail.com>
2023-04-24 11:02:54 -0700

    chore: add comments

--- kvdb.c
+++ kvdb.c
@@ -25,11 +25,15 @@ bool db_put(struct db *self, char *key, void *val) {
         return false;
     }
 
+    // generate log record
     struct log_record record = {.key = key, 
                                 .val = val, 
                                 .type = LOG_RECORD_NORMAL};
                             
+    // append to the active data file
     struct log_record_pos* pos = append_log_record(self, &record);
+
+    // update the corresponding log_record_pos in the cache
     self->index->put(self->index, key, pos);
     return true;
 }
@@ -39,13 +43,16 @@ char* db_get(struct db *self, char *key) {
         return NULL;
     }
 
+    // fetch the log record position from index
     struct log_record_pos* pos = self->index->get(self->index, key);
     if(pos == NULL) {
         return NULL;
     }
 
+    // select the data file to read
     struct data_file* df = self->active_data_file;
 
+    // according to the log record position, read the log record
     struct log_record* record = data_file_read(df, pos->offset);
     if (record && record->type == LOG_RECORD_NORMAL) {
         return record->val;
